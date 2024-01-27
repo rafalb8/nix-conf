779ad6496f4a69e114411ddd8d9447439085b389
Rafalb8 <rafalb8@hotmail.com>
2024-01-27 17:04:02 +0100

    Add autostart

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }:
 let
   cfg = config.modules.desktop;
+  autostart = import ../functions/autostart.nix config;
 in
 {
   imports = [
@@ -88,6 +89,14 @@ in
           };
         };
       };
+
+      # Autostart
+      home.file = autostart [
+        pkgs.steam
+        pkgs.discord
+        pkgs.solaar
+        (pkgs.easyeffects // { source = "com.github.wwmm.easyeffects.desktop"; })
+      ];
 
       programs = {
         # Configure alacritty
@@ -191,19 +200,21 @@ in
           fsType = "fuse.bindfs";
           options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
         };
-        aggregatedIcons = pkgs.buildEnv {
-          name = "system-icons";
-          paths = with pkgs; [
-            #libsForQt5.breeze-qt5  # for plasma
-            gnome.gnome-themes-extra
-          ];
-          pathsToLink = [ "/share/icons" ];
-        };
-        aggregatedFonts = pkgs.buildEnv {
-          name = "system-fonts";
-          paths = config.fonts.packages;
-          pathsToLink = [ "/share/fonts" ];
-        };
+        aggregatedIcons = pkgs.buildEnv
+          {
+            name = "system-icons";
+            paths = with pkgs; [
+              #libsForQt5.breeze-qt5  # for plasma
+              gnome.gnome-themes-extra
+            ];
+            pathsToLink = [ "/share/icons" ];
+          };
+        aggregatedFonts = pkgs.buildEnv
+          {
+            name = "system-fonts";
+            paths = config.fonts.packages;
+            pathsToLink = [ "/share/fonts" ];
+          };
       in
       {
         "/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
