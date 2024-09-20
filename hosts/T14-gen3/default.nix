0bd0b0d1728f23f71bd04dfcec7c444d16b08f17
Rafalb8 <rafalb8@hotmail.com>
2024-09-20 09:18:55 +0200

    Change tlp settings

--- hosts/T14-gen3/default.nix
+++ hosts/T14-gen3/default.nix
@@ -78,9 +78,15 @@
   services.tlp = {
     enable = true;
     settings = {
-      #Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      
+      # Charge tresholds. (Lenovo defaults)
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
     };
   };
 
