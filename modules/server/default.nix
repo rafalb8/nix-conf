1671941c87b79cb458fe29172099837d6d745748
Rafalb8 <rafalb8@hotmail.com>
2025-05-30 15:20:32 +0200

    Add script to check zpool temps

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -1,6 +1,16 @@
 { config, pkgs, lib, ... }:
 let
   cfg = config.modules.server;
+
+  ztemp = pkgs.writeShellScriptBin "ztemp" ''
+    DISKS=$(zpool status | grep -oP 'usb-[[:alnum:]_.-]+:0' | sort -u)
+    if [[ -z "$DISKS" ]]; then echo "No disk IDs found. Exiting."; exit 1; fi
+    for DISK in $DISKS; do
+        TEMP=$(sudo smartctl -a "/dev/disk/by-id/$DISK" 2>/dev/null |\
+          grep -iP '^\s*\d+\s+Temperature(_Celsius)?' | awk '{print $NF}')
+        echo "Disk: $DISK, Temperature: ''${TEMP:-N/A}°C"
+    done 
+  '';
 in
 {
   imports = [ ];
@@ -25,5 +35,11 @@ in
         zpool status -x
       '';
     };
+
+    # Add packages
+    environment.systemPackages = with pkgs; [
+      ztemp
+      smartmontools
+    ];
   };
 }
