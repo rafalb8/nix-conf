591a76d1b9b4d4cf331a3f9adb6ab22316ea011a
Rafalb8 <rafalb8@hotmail.com>
2025-06-13 16:18:32 +0200

    ...

--- modules/desktop/gaming/experiments.nix
+++ modules/desktop/gaming/experiments.nix
@@ -5,17 +5,13 @@ let
       env = {
         "MANGOHUD" = "0";
         "MANGOHUD_CONFIGFILE" = "/home/${config.user.name}/.config/MangoHud/MangoHud.conf";
-        "LD_PRELOAD" = ""; # Disable Steam OVerlay (--steam breaks gamescope)
+        "LD_PRELOAD" = ""; # Disable Steam Overlay (--steam breaks gamescope)
       };
       args = [
-        # "--steam"
         "--adaptive-sync"
         "--immediate-flips"
-        "--force-grab-cursor"
+        # "--force-grab-cursor"
         "--mangoapp"
-        "-w 3440 -h 1440"
-        "-W 2560 -H 1080"
-        "-r 75"
       ];
     in
     pkgs.writeShellScriptBin "playscope" ''
@@ -27,37 +23,6 @@ in
   environment.systemPackages = [ playscope ];
 
   programs = {
-    steam.gamescopeSession = {
-      enable = false;
-      env = {
-        STEAM_GAMESCOPE_VRR_SUPPORTED = "1";
-
-        STEAM_MANGOAPP_PRESETS_SUPPORTED = "1";
-        STEAM_USE_MANGOAPP = "1";
-        STEAM_DISABLE_MANGOAPP_ATOM_WORKAROUND = "1";
-        STEAM_MANGOAPP_HORIZONTAL_SUPPORTED = "1";
-
-        STEAM_GAMESCOPE_HAS_TEARING_SUPPORT = "1";
-        STEAM_GAMESCOPE_TEARING_SUPPORTED = "1";
-
-        STEAM_GAMESCOPE_HDR_SUPPORTED = "1";
-
-        STEAM_MULTIPLE_XWAYLANDS = "1";
-
-        STEAM_GAMESCOPE_DYNAMIC_FPSLIMITER = "1";
-        STEAM_GAMESCOPE_NIS_SUPPORTED = "1";
-        STEAM_GAMESCOPE_FANCY_SCALING_SUPPORT = "1";
-
-        STEAM_GAMESCOPE_COLOR_MANAGED = "1";
-        STEAM_GAMESCOPE_VIRTUAL_WHITE = "1";
-      };
-      args = [
-        "--mangoapp"
-        "-W 3840 -H 2160"
-        "-r 60"
-      ];
-    };
-
     gamescope.enable = true;
     gamescope.capSysNice = false;
 
