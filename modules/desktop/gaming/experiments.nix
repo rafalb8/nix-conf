48da2add6572aeba51cb1894f2e8fd47957d2816
Rafalb8 <rafalb8@hotmail.com>
2026-01-16 11:59:03 +0100

    Playscope v2

--- modules/desktop/gaming/experiments.nix
+++ modules/desktop/gaming/experiments.nix
@@ -2,32 +2,42 @@
 let
   cfg = config.modules.desktop;
 
-  playscope =
-    let
-      env = {
-        "MANGOHUD" = "0";
-        # "MANGOHUD_CONFIGFILE" = "~/.config/MangoHud/MangoHud.conf";
-        # "SDL_VIDEODRIVER" = "x11";
-      };
-      args = [
-        # "--backend sdl"
-        "--adaptive-sync"
-        "--immediate-flips"
-        "--mangoapp"
-        "--force-grab-cursor"
-        "-b"
-        "-W 2305"
-        "-H 1408"
-      ];
-    in
-    pkgs.writeShellScriptBin "playscope" ''
-      ${lib.custom.toExportShellVars env}
-      gamescope ${toString args} "$@"
-    '';
+  mkPlayscope = name: env: args: pkgs.writeShellScriptBin name ''
+    ${lib.custom.toExportShellVars env}
+
+    # Separate args: everything before -- is pre, everything after is post
+    pre=()
+    while [[ $# -gt 0 ]]; do
+      [[ "$1" == "--" ]] && { shift; break; }
+      pre+=("$1")
+      shift
+    done
+
+    # If no -- was found, what we thought was 'pre' is actually the game command
+    if [[ $# -eq 0 && ''${#pre[@]} -gt 0 ]]; then
+      exec gamescope ${toString args} -- env LD_PRELOAD="" "''${pre[@]}"
+    else
+      exec gamescope ${toString args} "''${pre[@]}" -- env LD_PRELOAD="" "$@"
+    fi
+  '';
+
+  env = { "MANGOHUD" = "0"; };
+  args = [
+    "--adaptive-sync"
+    "--immediate-flips"
+    "--mangoapp"
+    "--force-grab-cursor"
+    "-b"
+    "-W 2503"
+    "-H 1408"
+  ];
+
+  playscope = mkPlayscope "playscope" env args;
+  playscope-sdl = mkPlayscope "playscope-sdl" (env // { "SDL_VIDEODRIVER" = "x11"; }) (args ++ [ "--backend sdl" ]);
 in
 {
   config = lib.mkIf cfg.gaming.enable {
-    environment.systemPackages = [ playscope ];
+    environment.systemPackages = [ playscope playscope-sdl ];
 
     programs = {
       # VR
