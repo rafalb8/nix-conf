88704c2b4b43b0579e6e90c1f323c05c392ac7f2
Rafalb8 <rafalb8@hotmail.com>
2026-02-08 10:15:37 +0100

    Update eden.nix

--- packages/eden.nix
+++ packages/eden.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, makeDesktopItem }:
 stdenv.mkDerivation (final: {
   pname = "eden";
-  version = "0.0.4-rc3";
+  version = "0.1.1";
 
   src = fetchurl {
     url = "https://github.com/eden-emulator/Releases/releases/download/v${final.version}/Eden-Linux-v${final.version}-amd64-clang-pgo.AppImage";
-    sha256 = "0dkk90g1dy2rymc0mg1qq6r75cibghqxvkblpa79ym3akgpv4pcx"; # nix-prefetch-url <url>
+    sha256 = "0pv4i4ih4m2zrrdpfwkwhgb8x7i7a754si7pdbv9rrrz6g9pbg43"; # nix-prefetch-url <url>
   };
 
   desktopItem = makeDesktopItem {
