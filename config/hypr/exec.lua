d9b2bc775c41d9cc0cb18046632c633ed955edfa
Rafalb8 <rafalb8@hotmail.com>
2026-06-29 20:43:54 +0200

    Add `Restart=on-failure` to `hyprland.start` apps

--- config/hypr/exec.lua
+++ config/hypr/exec.lua
@@ -1,10 +1,13 @@
-hl.on("hyprland.start", function()
-    hl.exec_cmd("uwsm-app -s s -t service -- waybar")
-    hl.exec_cmd("uwsm-app -s s -t service -- swaync")
-    hl.exec_cmd("uwsm-app -s s -t service -- elephant")
-    hl.exec_cmd("uwsm-app -s s -t service -- hypridle")
-    hl.exec_cmd("uwsm-app -s s -t service -- sway-audio-idle-inhibit")
-    hl.exec_cmd("uwsm-app -s s -t service -- walker --gapplication-service")
+local uwsm_session = "uwsm-app -s s -t service -p 'Restart=on-failure' -- "
+local uwsm_background = "uwsm-app -s b -t service -p 'Restart=on-failure' -- "
 
-    hl.exec_cmd("uwsm-app -s b -t service -- hyprpaper")
+hl.on("hyprland.start", function()
+    hl.exec_cmd(uwsm_session .. "waybar")
+    hl.exec_cmd(uwsm_session .. "swaync")
+    hl.exec_cmd(uwsm_session .. "elephant")
+    hl.exec_cmd(uwsm_session .. "hypridle")
+    hl.exec_cmd(uwsm_session .. "sway-audio-idle-inhibit")
+    hl.exec_cmd(uwsm_session .. "walker --gapplication-service")
+
+    hl.exec_cmd(uwsm_background .. "hyprpaper")
 end)
